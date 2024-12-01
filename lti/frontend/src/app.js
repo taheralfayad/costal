e2194813399e9f7fada912f3f1d00a4024f97777
Daniel Gonzalez <103468969+lvl2daniel@users.noreply.github.com>
2024-12-01 16:36:03 -0500

    Create simple routing for current textbook view landscape

--- lti/frontend/src/app.js
+++ lti/frontend/src/app.js
@@ -1,12 +1,31 @@
+import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TextbookView from './components/TextbookView/TextbookView.tsx';
+import TextbookList from './components/TextbookView/TextbookList.tsx';
+import PDFViewer from './components/TextbookView/PDFViewer.tsx';
 
 function App() {
   return (
-    <div className="App">
-      <h1>COSTAL</h1>
-      <p>This is the application home page</p>
-    </div>
+      <Routes>
+      <Route path="/list" element={<TextbookList />} />
+      <Route path="/textbook/:id" 
+             element={
+             <TextbookView
+             pdfFile="/sample.pdf"
+             title="Introduction to Python Programming"
+             author="Udayan Das" 
+             />}
+            />
+        <Route
+          path="/textbook"
+          element={
+            <PDFViewer pdfFile="/sample.pdf"/>
+          }
+        />
+      </Routes>
   );
 }
 
 export default App;
+ 
