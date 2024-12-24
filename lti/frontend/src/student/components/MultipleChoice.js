4a56f15f9e9e23d43eb73dcd894648e92fdf0282
Nicole Zolnier <nzolnier@gmail.com>
2024-12-24 16:23:08 -0300

    add multiple choice question

---
+++ lti/frontend/src/student/components/MultipleChoice.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button, Checkbox } from '../../design-system';
+
+const MultipleChoice = ({title, question, options}) => {
+  return (
+    <article className='w-[90%] mx-auto'>
+      <header className="flex justify-between items-center mb-4">
+        <h3 className="text-slate-900 font-medium text-base">{title}</h3>
+      </header>
+      <main className="w-full border border-slate-300 rounded-lg p-8 bg-white flex flex-col gap-4">
+        <h4 className="text-slate-900 font-medium uppercase text-base">
+          Question
+        </h4>
+        <section className='flex flex-col gap-3'>
+            <label className='block mb-2 text-sm font-medium text-gray-700'>{question}</label>
+            {options.map((option) => {
+                return <Checkbox label={option} />
+            })}
+        </section>
+        <section className='flex justify-end gap-2'>
+            <Button label='More instruction' type='outline' />
+            <Button label='Submit' />
+        </section>
+      </main>
+    </article>
+  );
+};
+
+export default MultipleChoice;
+
--- lti/frontend/src/student/pages/Assignment.js
+++ lti/frontend/src/student/pages/Assignment.js
@@ -3,6 +3,7 @@ import Arrow from '../../assets/arrow-left.svg';
 import Menu from '../../assets/menu-alt.svg';
 import { Title } from '../../design-system';
 import Loader from '../components/Loader';
+import MultipleChoice from '../components/MultipleChoice';
 import ProgressBar from '../components/ProgressBar';
 import Video from '../components/Video';
 import Writing from '../components/Writing';
@@ -23,7 +24,7 @@ const Assignment = () => {
       </header>
 
       <section className='bg-[#f8f8f8] h-screen pt-8'>
-        <Writing title='Lorem Ipsum' question='Round 619,348 to the nearest ten thousand.' placeholder='Placeholder' />
+        <MultipleChoice title='Lorem Ipsum' question='Choose the left and right options' options={['Wrong option', 'Right option', 'Wrong option', 'Left option']} />
       </section>
     </main>
   );
