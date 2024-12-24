26f5cf97fc3c965ca30808bacf512d9c4f7541dd
Nicole Zolnier <nzolnier@gmail.com>
2024-12-24 16:28:34 -0300

    fix

--- lti/frontend/src/student/components/MultipleChoice.js
+++ lti/frontend/src/student/components/MultipleChoice.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Button, Checkbox } from '../../design-system';
+import DotsVertical from '../../assets/dots-vertical.svg';
 
 const MultipleChoice = ({title, question, options}) => {
   return (
     <article className='w-[90%] mx-auto'>
       <header className="flex justify-between items-center mb-4">
         <h3 className="text-slate-900 font-medium text-base">{title}</h3>
+        <button className="text-slate-900">
+          <div className="flex flex-col space-y-1">
+            <DotsVertical />
+          </div>
+        </button>
       </header>
       <main className="w-full border border-slate-300 rounded-lg p-8 bg-white flex flex-col gap-4">
         <h4 className="text-slate-900 font-medium uppercase text-base">
