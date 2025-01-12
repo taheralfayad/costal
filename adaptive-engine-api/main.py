4a73d998e7a0a0185b211c99bdd45a72d73adfe8
taher <taheralfayad@gmail.com>
2025-01-12 14:34:01 -0500

    add MAB reward function and context

--- adaptive-engine-api/main.py
+++ adaptive-engine-api/main.py
@@ -7,7 +7,6 @@ import uvicorn
 import pandas as pd
 import pyBKT.models as pybkt
 from mabwiser.mab import MAB, LearningPolicy, NeighborhoodPolicy
-from sklearn.preprocessing import StandardScaler
 
 app = FastAPI()
 
@@ -22,8 +21,13 @@ class RunModelOnResponseRequest(BaseModel):
     assignment_id: int
     module_id: int
     skill_name: str
+    seconds_taken: int
+    percentage_of_last_five_correct: int
+    difficulty: int
+    hints_used: int
     correct: int
     questions: list
+    question_id: int
 
 class FitModelRequest(BaseModel):
     user_id: int
@@ -40,20 +44,10 @@ class FitModelRequest(BaseModel):
 def run_model_on_response(request: RunModelOnResponseRequest):
     model = pybkt.Model(seed=42, num_fits=1)
     model.load('./models/bkt_model_{course_id}_{module_id}.pkl'.format(course_id=request.course_id, module_id=request.module_id))
+    with open("./models/mab_model_{course_id}_{assignment_id}_{user_id}.pkl".format(course_id=request.course_id, assignment_id=request.assignment_id, user_id=request.user_id), "rb") as f:
+        mab = pickle.load(f)
 
-    try:
-        with open("./models/mab_model_{course_id}_{assignment_id}_{user_id}.pkl".format(course_id=request.course_id, assignment_id=request.assignment_id, user_id=request.user_id), "rb") as f:
-            mab = pickle.load(f)
-    except:
-        mab = MAB(arms=[], learning_policy=LearningPolicy.LinUCB(alpha=1.25, l2_lambda=1))
-    
-
-    if len(mab.arms) < len(request.questions):
-        set_of_mab_arms = set(mab.arms)
-
-        for question in request.questions:
-            if question not in set_of_mab_arms:
-                mab.add_arm(question
+    set_of_mab_arms = set(mab.arms)
 
     data = pd.DataFrame(
         {
@@ -71,13 +65,38 @@ def run_model_on_response(request: RunModelOnResponseRequest):
     model.fit(data=data)
     predictions_after = model.predict(data=data)
 
-    arms = request.questions
-
-    mab = 
-
     delta = predictions_after.iloc[0]['state_predictions'] - predictions_before.iloc[0]['state_predictions']
 
+    if request.question_id not in set_of_mab_arms:
+        mab.add_arm(question)
+
+    arm_context = pd.DataFrame(
+        {
+            'skill_id': [request.skill_id],
+            'seconds_taken': [request.seconds_taken],
+            'hints_used': [request.hints_used],
+            'difficulty': [request.difficulty],
+            'percentage_of_last_five_correct': [request.percentage_of_last_five_correct]
+            'bkt_prediction': [predictions_after.iloc[0]['state_predictions']]
+        }
+    )
+
+    # Reward function is calculated based on the difference in state prediction
+    # It is also magnified or reduced based on the difficulty of the question
+    if delta > 0:
+        multiplier = 1 + (difficulty - 1) * 0.25
+    else:
+        multiplier = 1 - (difficulty - 1) * 0.25
+
+    reward = pd.Series([delta * multiplier])
+
+    mab.partial_fit(decisions=[request.question_id], rewards=reward, context=arm_context)
+
+    next_question = mab.predict(context=arm_context)
+
     model.save('./models/bkt_model_{course_id}_{user_id}.pkl'.format(course_id=request.course_id, user_id=request.user_id))
+    with open("./models/mab_model_{course_id}_{assignment_id}_{user_id}.pkl".format(course_id=request.course_id, assignment_id=request.assignment_id, user_id=request.user_id), "wb") as f:
+        pickle.dump(mab, f)
 
     prediction_json = {
         "state_prediction": predictions.iloc[0]['state_predictions'],
